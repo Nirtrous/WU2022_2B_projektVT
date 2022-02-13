5235c20e468e0bb4d54d42d4053dd48a5bb6deb8
Jakob-Andersson-Nti-Johanneberg <92986731+Nirtrous@users.noreply.github.com>
2022-02-13 18:23:53 +0100

    v1 temp_mobile_nav

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const banana_burger = document.querySelector('.banana_burger')
+const nav = document.querySelector('.nav_links')
+
+function toggleMenu() {
+    nav.classList.toggle('nav-active')
+    banana_burger.classList.toggle('toggle')
+}
+
+banana_burger.addEventListener('click', toggleMenu)
