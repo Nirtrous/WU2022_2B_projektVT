3951f8b15074cbb1155d1dd5f8588adae4e55782
Jakob-Andersson-Nti-Johanneberg <92986731+Nirtrous@users.noreply.github.com>
2022-03-30 21:52:10 +0200

    v12.5 beginning 5th page

--- js/script.js
+++ js/script.js
@@ -1,17 +1,28 @@
+
+
+
+
+
+
+
+const newsletter = document.querySelector('#newsletter_button')
+
+
+function toggleFill() {
+    newsletter.classList.toggle('toggle') 
+}
+
+newsletter.addEventListener('click', toggleFill)
+
+
 const banana_burger = document.querySelector('.banana_burger')
 const nav = document.querySelector('.nav_links')
+
 
 function toggleMenu() {
     nav.classList.toggle('nav-active')
     banana_burger.classList.toggle('toggle')
+    correct.classList.toggle('toggle')
 }
 
 banana_burger.addEventListener('click', toggleMenu)
-
-const newsletter = document.querySelector('#newsletter_button')
-
-function togglefill() {
-    newsletter.classList.toggle('toggle') 
-}
-
-newsletter.addEventListener('click', togglefill)
